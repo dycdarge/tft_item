4daadce7064bbe60a9f451a7592024d270e833cf
kwarac <k.waraczynski@tarent.de>
2019-06-27 21:49:51 +0200

    Updates

---
+++ precache-manifest.91e932a36466382a1df03e163f82322f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7114d9dbf6d2fd987e08e32e8d2d7ae0",
+    "url": "/tft_item/index.html"
+  },
+  {
+    "revision": "b3392d06805897bedb8d",
+    "url": "/tft_item/static/css/main.68a52652.chunk.css"
+  },
+  {
+    "revision": "f43c4bfee2815856d4c9",
+    "url": "/tft_item/static/js/2.e9bf0032.chunk.js"
+  },
+  {
+    "revision": "b3392d06805897bedb8d",
+    "url": "/tft_item/static/js/main.92b6e2ee.chunk.js"
+  },
+  {
+    "revision": "b355b6718210c5f3d792",
+    "url": "/tft_item/static/js/runtime~main.aa49c0de.js"
+  },
+  {
+    "revision": "2810e4b951e7f244851b2b44e2db7470",
+    "url": "/tft_item/static/media/Chain_Vest_item.2810e4b9.png"
+  },
+  {
+    "revision": "0f0daf6662f2899095944214381dac5c",
+    "url": "/tft_item/static/media/Negatron_Cloak_item.0f0daf66.png"
+  },
+  {
+    "revision": "df3c82a7352bb3feaee92188f8e0212c",
+    "url": "/tft_item/static/media/Recurve_Bow_item.df3c82a7.png"
+  },
+  {
+    "revision": "d702cad03efa795f0572036615385dbf",
+    "url": "/tft_item/static/media/bf_sword_list.d702cad0.jpg"
+  },
+  {
+    "revision": "c5c87d4d9b1426a4c10c40f9869ce36b",
+    "url": "/tft_item/static/media/chainvest_list.c5c87d4d.jpg"
+  },
+  {
+    "revision": "b3c8a065497639c5bcbd88533267987c",
+    "url": "/tft_item/static/media/giantsbelt_list.b3c8a065.jpg"
+  },
+  {
+    "revision": "00658b3570919c4435ae60d350b9bd2e",
+    "url": "/tft_item/static/media/largerod_list.00658b35.jpg"
+  },
+  {
+    "revision": "bd4184ecf66005eea55cb38745f45917",
+    "url": "/tft_item/static/media/negatron_list.bd4184ec.jpg"
+  },
+  {
+    "revision": "d4a05ca9aa936a9332a4a0e7f727c3c9",
+    "url": "/tft_item/static/media/recurve_list.d4a05ca9.jpg"
+  },
+  {
+    "revision": "e3da1cfa955392a96feb44b3f022028b",
+    "url": "/tft_item/static/media/spatula_list.e3da1cfa.jpg"
+  },
+  {
+    "revision": "558a7b2eb14939d553b7b73d7fb06afc",
+    "url": "/tft_item/static/media/tear_list.558a7b2e.jpg"
+  }
+]);
